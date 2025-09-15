a9616f3781763c1d6151e078371488f827e2bec8
JFL <94jingfang@gmal.com>
2025-09-15 15:45:28 +0800

    modify_main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from pythonweb!")
-
-
-if __name__ == "__main__":
-    main()
+#todo:請建立一個Flask應用程式，並在根路由('/')回傳"Hello, Flask!"字串。
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return "Hello, Flask!"
+if __name__ == '__main__':
+    app.run(debug=True)
