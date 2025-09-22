e560c0da6bcba6df9fc0f52410463fcf4baf9bb1
JFL <94jingfang@gmal.com>
2025-09-22 11:22:08 +0800

    lesson2_debug_2

--- lesson2/lesson2_1.py
+++ lesson2/lesson2_1.py
@@ -1,16 +1,14 @@
 def add(a, b):
     result = a + b
-    return result
+    return result   #回傳值至呼叫add function位置
 
 def main():
     x = 10
     y = 20
-    sum_result = add(x, y)
-    print(f"{x} + {y} = {sum_result}")
-
-main()
+    sum_result = add(x, y)  #呼叫add function 
+    print(f"{x} + {y} = {sum_result}") #回傳值至呼叫mian function位置
 
 if __name__ == "__main__":
-    print("這是pthon直接執行")
+    main()  #呼叫mian function (程式起始點)
 else:
     print("這不是python直接執行")
