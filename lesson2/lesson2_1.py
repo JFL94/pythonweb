1ec66ec50bf4aaf87c5d935c88e306e99a9e27fc
JFL <94jingfang@gmal.com>
2025-09-22 11:00:30 +0800

    lesson2_debug

--- lesson2/lesson2_1.py
+++ lesson2/lesson2_1.py
@@ -1,3 +1,15 @@
+def add(a, b):
+    result = a + b
+    return result
+
+def main():
+    x = 10
+    y = 20
+    sum_result = add(x, y)
+    print(f"{x} + {y} = {sum_result}")
+
+main()
+
 if __name__ == "__main__":
     print("這是pthon直接執行")
 else:
