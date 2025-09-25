0dbf83cb5381e3a636d09a9792d9a8deb6db4a0d
JFL <94jingfang@gmal.com>
2025-09-25 15:04:53 +0800

    lesson3_3_function

--- lesson3/lesson3_3.py
+++ lesson3/lesson3_3.py
@@ -1,39 +1,42 @@
 import requests
 
-url = 'https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/json?page=0&size=1000'
+def download_youbike_data():
+    url = 'https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/json?page=0&size=1000'
 
-'''
-response = requests.get(url)
-print(type(response))
-
-if response.status_code == 200:
-    print("下載成功")
-    print("下載的內容如下")
-    #print(response.text)
-    #print(type(response.json()))
-    for item in response.json():
-        print(item)
-else:
-    print("下載失敗")
-'''
-
-try:
+    '''
     response = requests.get(url)
-    response.raise_for_status()
-    try:
-        data = response.json()
-    except requests.exceptions.JSONDecodeError as jsonError:
-        print(f"發生轉換格是錯誤:jsonError")
-except requests.exceptions.HTTPError as err_http:
-    print(f"http error:{err_http}")
-except requests.exceptions.ConnectionError as err_conn:
-    print(f"發生連線錯誤(例如 DNS 查詢失敗、連線被拒):{err_conn}")
-except requests.exceptions.ConnectTimeout as err_timeout:
-    print(f"請求超時:{err_timeout}")
-except requests.exceptions.RequestException as err:
-    # 這是所有 requests 例外的父類別，可以用來捕捉其他未預期的錯誤
-    print(f"發生未預期的請求錯誤: {err}")
-else:
-    print(f"沒有出錯:\n{data}")
+    print(type(response))
 
-print("\n不管有沒有出錯都會執行")
+    if response.status_code == 200:
+        print("下載成功")
+        print("下載的內容如下")
+        #print(response.text)
+        #print(type(response.json()))
+        for item in response.json():
+            print(item)
+    else:
+        print("下載失敗")
+    '''
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        try:
+            data = response.json()
+        except requests.exceptions.JSONDecodeError as jsonError:
+            print(f"發生轉換格是錯誤:jsonError")
+    except requests.exceptions.HTTPError as err_http:
+        print(f"http error:{err_http}")
+    except requests.exceptions.ConnectionError as err_conn:
+        print(f"發生連線錯誤(例如 DNS 查詢失敗、連線被拒):{err_conn}")
+    except requests.exceptions.ConnectTimeout as err_timeout:
+        print(f"請求超時:{err_timeout}")
+    except requests.exceptions.RequestException as err:
+        # 這是所有 requests 例外的父類別，可以用來捕捉其他未預期的錯誤
+        print(f"發生未預期的請求錯誤: {err}")
+    else:
+        print(f"沒有出錯:\n{data}")
+
+if __name__ == "__main__":
+    download_youbike_data()
+
