09fcd187e3b6ef4d0dc26630c31ea67c8f9ee2f3
JFL <94jingfang@gmal.com>
2025-10-30 16:31:26 +0800

    lesson11

--- lesson11/knn/app.py
+++ lesson11/knn/app.py
@@ -1,4 +1,9 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,render_template, jsonify,request
+from sklearn.datasets import load_iris
+from sklearn.linear_model import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
+import numpy as np
 
 knn_bp = Blueprint(
     'knn',
@@ -11,3 +16,14 @@ knn_bp = Blueprint(
 @knn_bp.route('/knn_index')
 def knn_index():
     return render_template('knn.html')
+
+@knn_bp.route('/api/data')
+def knn_data():
+    """knn 分類 API - 使用鳶尾花資料集"""
+    iris = load_iris()
+    print(iris)
+    return jsonify({'sucess':True})
+
+@knn_bp.route('/api/predict')
+def knn_predict():
+    return jsonify({'sucess':True})
