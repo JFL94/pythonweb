b3a260f336b117f8cdb3d5dac5f81d7f1e6c6a66
JFL <94jingfang@gmal.com>
2025-10-30 16:03:01 +0800

    modify

---
+++ lesson11/static/js/knn.js
@@ -0,0 +1,10 @@
+// 1.頁面戴入完成後才執行
+document.addEventListener('DOMContentLoaded', function () {
+    //2.固定使用花瓣長度(2)和花瓣寬度(3)
+    loadKnnData();
+
+})
+
+function loadKnnData() {
+    console.log("loadKnnData")
+} 
